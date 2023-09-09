10b10065e6418c563574aa647c839f04c14c53ab
HumayunKabir18 <hasibpstu17@gmail.com>
2023-09-10 03:20:12 +0600

    setting changes

--- stockMarket/settings.py
+++ stockMarket/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ff-&jzqy!!=wdt1yowked9iplgyn!t$!g93htzi=fj8@6(*&nh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
