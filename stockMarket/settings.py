233757dce769025cc215fe79bcfebd1090befc26
HumayunKabir18 <hasibpstu17@gmail.com>
2023-09-10 06:07:07 +0600

    finishing

--- stockMarket/settings.py
+++ stockMarket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ff-&jzqy!!=wdt1yowked9iplgyn!t$!g93htzi=fj8@6(*&nh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
